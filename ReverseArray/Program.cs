72a8fbe065ef0ac0fe73636ec3fdbc8a07192c7f
Archana Dixit <archanavdixit@yahoo.com>
2018-01-08 13:58:04 -0600

    Add project files.

---
+++ ReverseArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] arr_temp = Console.ReadLine().Split(' ');
+        int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+        int[] arrReverse = new int[n];
+        int i;
+        //Loop to access Array elements//
+        for( i=n-1; i>=0;i--)
+        {
+            arrReverse[i] = arr[i];
+            Console.Write(arrReverse[i]);
+            Console.Write(" ");
+        }
+        
+
+        Console.ReadLine();
+    }
+}
